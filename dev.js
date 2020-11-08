12070ff61953d4c230d8c4119f74737e995661ba
AlexandreFeren <39268361+AlexandreFeren@users.noreply.github.com>
2020-11-08 14:49:09 -0500

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,35 +1,35 @@
 function main(gameState, side) {
-  console.log(gameState);
+	console.log(gameState);
 	console.log(side);
 	console.log(gameState.teamStates[side];
 	console.log(gameState.getOwnPropertyNames)
 	const myTeam = gameState.teamStates[side];
-  const possibleMoves = [];
-  const [rowSize, colSize] = gameState.boardSize;
-  return new Promise((resolve, reject) => {
-    const callback = () => resolve(
-      myTeam.reduce((moveSet, member) => {
-        if (member.isDead) {
-          moveSet.push('none');
-        } else {
-          possibleMoves.push('none');
+	const possibleMoves = [];
+	const [rowSize, colSize] = gameState.boardSize;
+	return new Promise((resolve, reject) => {
+		const callback = () => resolve(
+			myTeam.reduce((moveSet, member) => {
+				if (member.isDead) {
+					moveSet.push('none');
+				} else {
+					possibleMoves.push('none');
 
-					const [row, col] = member.coord;
-          if (row > 1) possibleMoves.push('north');
-          if (row < rowSize - 1)  possibleMoves.push('south');
-          if (col > 1) possibleMoves.push('west');
-          if (col < colSize - 1)  possibleMoves.push('east');
-          moveSet.push(possibleMoves[Math.floor(Math.random() * possibleMoves.length)]);
-          possibleMoves.length = 0;
-        }
-        return moveSet;
-      }, [])
-    );
+			const [row, col] = member.coord;
+					if (row > 1) possibleMoves.push('north');
+					if (row < rowSize - 1)	possibleMoves.push('south');
+					if (col > 1) possibleMoves.push('west');
+					if (col < colSize - 1)	possibleMoves.push('east');
+					moveSet.push(possibleMoves[Math.floor(Math.random() * possibleMoves.length)]);
+					possibleMoves.length = 0;
+				}
+				return moveSet;
+			}, [])
+		);
 
-    // we are returning a timeout here to test limiting execution time on the sandbox side.
-    return setTimeout(
-      callback
-    , 0); // test timeout of player script for limiting execution time.
+		// we are returning a timeout here to test limiting execution time on the sandbox side.
+		return setTimeout(
+			callback
+		, 0); // test timeout of player script for limiting execution time.
 
-    })
+		})
 }
