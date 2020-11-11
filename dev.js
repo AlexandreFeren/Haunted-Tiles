11e8a0f2239c1afbba0f2fa9f6850b390e3cbf1d
AlexandreFeren <39268361+AlexandreFeren@users.noreply.github.com>
2020-11-11 01:05:56 -0500

    Update dev.js

--- dev.js
+++ dev.js
@@ -21,13 +21,20 @@ function main(gameState, side) {
 	//console.log(minimax(gameState, allMoves, side));
 	console.log("in main");
 	console.log(gameState);
-	b = gameState.tileStates;
-	console.log(b);
-	b[0][0] = -1;
-	console.log("testing board copy");
-	console.log(b);
+	//b = gameState.tileStates;
+	//console.log(b);
+	//b[0][0] = -1;
+	//console.log("testing board copy");
+	//console.log(b);
+	b = []
+	for (i = 0; i < gameState.tileStates; i++){
+		b.push([]);
+		for (j = 0; j < gameState.tileStates[i]; j++){
+			b.push(gameState.tileStates[i][j]);
+		}
+	}
 	
-	console.log(getGameState([gameState.tileStates, [gameState.teamStates.home, gameState.teamStates.away]], [allMoves[0][1], allMoves[1][1], allMoves[2][1]], side));
+	console.log(getGameState([b, [gameState.teamStates.home, gameState.teamStates.away]], [allMoves[0][1], allMoves[1][1], allMoves[2][1]], side));
 	return minimax(gameState, allMoves, side);
 	// we are returning a timeout here to test limiting execution time on the sandbox side.
 }
