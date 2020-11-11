46c4b643e3f0d02b7e6f1f903a8ca0631289f53b
AlexandreFeren <39268361+AlexandreFeren@users.noreply.github.com>
2020-11-11 09:34:15 -0500

    Update dev.js

--- dev.js
+++ dev.js
@@ -153,18 +153,18 @@ function getGameState(gameState, move, side, toMoveStart = 0){
 			//console.log(gameState[0][gameState[1][0][i].coord[0]][gameState[1][0][i].coord[1]]);
 			if (move[i] == 'north') {
 				gameState[0][gameState[1][0][i][0]-1][gameState[1][0][i][1]] -= 1;
-				gameState[1][0][i].coord[0]--;
+				gameState[1][0][i][0]--;
 			}else if (move[i] == 'south') {
 				console.log(gameState[0]);
 				console.log(gameState[1][0][i]);
 				gameState[0][gameState[1][0][i][0]+1][gameState[1][0][i][1]] -= 1;
-				gameState[1][0][i].coord[0]++;
+				gameState[1][0][i][0]++;
 			}else if (move[i] == 'west') {
 				gameState[0][gameState[1][0][i][0]][gameState[1][0][i].coord[1]-1] -= 1;
-				gameState[1][0][i].coord[1]--;
+				gameState[1][0][i][1]--;
 			}else if (move[i] == 'east') {
 				gameState[0][gameState[1][0][i][0]][gameState[1][0][i][1]+1] -= 1;
-				gameState[1][0][i].coord[1]++;
+				gameState[1][0][i][1]++;
 			}else{
 				gameState[0][gameState[1][0][i][0]][gameState[1][0][i][1]] -= 1;
 			}
@@ -172,16 +172,16 @@ function getGameState(gameState, move, side, toMoveStart = 0){
 		if (side == 'away' || side == ""){
 			if (move[i] == 'north') {
 				gameState[0][gameState[1][1][i][0]-1][gameState[1][1][i][1]] -= 1;
-				gameState[1][1][i].coord[0]--;
+				gameState[1][1][i][0]--;
 			}else if (move[i] == 'south') {
 				gameState[0][gameState[1][1][i][0]+1][gameState[1][1][i][1]] -= 1;
-				gameState[1][1][i].coord[0]++;
+				gameState[1][1][i][0]++;
 			}else if (move[i] == 'west') {
 				gameState[0][gameState[1][1][i][0]][gameState[1][1][i][1]-1] -= 1;
-				gameState[1][1][i].coord[1]--;
+				gameState[1][1][i][1]--;
 			}else if (move[i] == 'east') {
 				gameState[0][gameState[1][1][i][0]][gameState[1][1][i][1]+1] -= 1;
-				gameState[1][1][i].coord[1]++;
+				gameState[1][1][i][1]++;
 			}else{
 				gameState[0][gameState[1][1][i][0]][gameState[1][1][i][1]] -= 1;
 			}
