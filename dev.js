d37f87c9027ccee2db117a8df2368d8c428b3d52
AlexandreFeren <39268361+AlexandreFeren@users.noreply.github.com>
2020-11-13 15:36:17 -0500

    Update dev.js

--- dev.js
+++ dev.js
@@ -121,14 +121,12 @@ function value(gameState, side){
 	for (i = 0; i < 3; i++){
 		
 		//should mark team member as dead. As this is passed by reference, it should propagate
-		/*
 		if (gameState[0][teams[0][i][0]][teams[0][i][1]] <= 1){
 			teams[0][i][2] = true;
 		}
 		if (gameState[0][teams[1][i][0]][teams[1][i][1]] <= 1){
 		    	teams[1][i][2] = true;
 		}
-		*/
 		//check death status for each team
 		if (teams[0][i][2]){
 			val -= 1000;
