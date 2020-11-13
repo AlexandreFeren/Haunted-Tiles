721a531e2ceacfa8f76dd77c53e7faec70b02247
AlexandreFeren <39268361+AlexandreFeren@users.noreply.github.com>
2020-11-13 15:51:08 -0500

    remove **2 in distance check

--- dev.js
+++ dev.js
@@ -164,8 +164,7 @@ function value(gameState, side){
 			if (!teams[0][i][2]){
 				if (!teams[1][j][2]){
 					//manhattan distance between closest pieces
-					//squared to prioritize closing distance in the 'long' direction may be an idea for future
-					tempVal.push((Math.abs(teams[0][i][0]-teams[1][j][0])-2)**2 + (Math.abs(teams[0][i][1]-teams[1][j][1])-2)**2);
+					tempVal.push((Math.abs(teams[0][i][0]-teams[1][j][0])-2) + (Math.abs(teams[0][i][1]-teams[1][j][1])-2));
 				}
 			}
 		}
